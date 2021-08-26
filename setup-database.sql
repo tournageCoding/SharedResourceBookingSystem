afabecb67ada006fc9c98d46c5f7acf53698da86
Jakob <jakobharvey1212@gmail.com>
2021-08-26 17:11:24 +1200

    converted default db to booked_sessions

--- setup-database.sql
+++ setup-database.sql
@@ -1,8 +1,9 @@
-CREATE TABLE papers (
-  code varchar(7),
+CREATE TABLE booked_sessions (
   name varchar(50) NOT NULL,
-  PRIMARY KEY (code)
+  court int NOT NULL CHECK (court == 1 OR court == 2),
+  session DATETIME NOT NULL,
+  PRIMARY KEY (court, session)
 );
 
-INSERT INTO papers VALUES ('COSC326','Effective Programming');
-INSERT INTO papers VALUES ('COSC349','Cloud Computing Architecture');
+INSERT INTO booked_sessions VALUES ('Sam Johnson', 1, '2021-08-27 11:00:00');
+INSERT INTO booked_sessions VALUES ('Ben Dover', 2, '2021-08-26 12:00:00');
