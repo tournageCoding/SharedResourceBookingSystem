269ae5e548bd8008ea02bf65d2561b172ffbecad
Jakob <jakobharvey1212@gmail.com>
2021-09-02 11:51:24 +1200

    removed seconds from adminWebsite

--- www/adminWebsite.php
+++ www/adminWebsite.php
@@ -32,10 +32,10 @@ $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
 
 $nameI = $_POST["nameI"];
 $courtI = $_POST["courtI"];
-$sessionI = $_POST["sessionI"];
+$sessionI = $_POST["sessionI"].":00";
 $nameD = $_POST["nameD"];
 $courtD = $_POST["courtD"];
-$sessionD = $_POST["sessionD"];
+$sessionD = $_POST["sessionD"].":00";
 
 $idD = $_POST["idD"];
 $idI = $_POST["idI"];
@@ -78,7 +78,7 @@ $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
 $q = $pdo->query("SELECT * FROM booked_sessions ORDER BY session ASC");
 
 while($row = $q->fetch()){
-  echo "<tr><td>".$row["name"]."</td><td>".$row["court"]."</td><td>".$row["session"]."</td></tr>\n";
+  echo "<tr><td>".$row["name"]."</td><td>".$row["court"]."</td><td>".substr($row["session"], 0, 16)."</td></tr>\n";
 }
 ?>
 </table>
@@ -89,7 +89,7 @@ while($row = $q->fetch()){
 Name: <input type="text" name="nameI"><br><br>
 Court Number: <input type="number" name="courtI" min="1" max="2"><br><br>
 Date and Time: <input type="text" name="sessionI"
-		  pattern="[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}" required><br><br>
+		  pattern="[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}" required><br><br>
 <input type="submit">
 </form>
 
@@ -99,7 +99,7 @@ Date and Time: <input type="text" name="sessionI"
 Name: <input type="text" name="nameD"><br><br>
 Court Number: <input type="number" name="courtD" min="1" max="2"><br><br>
 Date and Time: <input type="text" name="sessionD"
-		  pattern="[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}" required><br><br>
+		  pattern="[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}" required><br><br>
 <input type="submit">
 </form>
 
